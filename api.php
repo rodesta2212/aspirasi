fc5edf1ba30b455b2d2e072a21c86a12d31fda33
rodesta2212 <rodesta2212@gmail.com>
2022-08-11 10:35:46 +0700

    contoh penggunaan web service

--- api.php
+++ api.php
@@ -5,3 +5,6 @@ Get API :
 <a href="wsLaporanPerangkatan.php">Laporan Grafik Aspirasi Berdasarkan Angkatan</a>
 <br/>
 <a href="wsLaporanHasilAdvokasi.php">Laporan Hasil Advokasi</a>
+<br/>
+<br/>
+<a href="viewJSONHasilAdvokasi.php">Contoh penggunaan web service pada Laporan Hasil Advokasi</a>
